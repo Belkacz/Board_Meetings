274d822a489d3cfda01b46352fbb92f142e656c2
Łukasz Belka <lukasz.belka@fujitsu.com>
2023-07-13 19:21:11 +0200

    Act XV: Edit Task works, also ngDestroy unsubscribe New Task and Edit Task Dialog subscritpion

--- src/app/new-meeting-page/tasks/tasks.component.ts
+++ src/app/new-meeting-page/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { DialogFormComponent } from 'src/app/dialog-form/dialog-form.component';
 import { Task } from 'src/app/shared/interfaces';
 
@@ -15,15 +16,17 @@ export class TasksComponent implements OnInit, OnDestroy {
   public tasksList: Task[];
   public showTaskMenu: boolean;
   public dialogTitle: string;
+  private dialogEditSub!: Subscription;
+  private newTaskSub!: Subscription
 
-  constructor( public dialog: MatDialog ) {
+  constructor(public dialog: MatDialog) {
     this.tasksList = [];
     this.showTaskMenu = false;
     this.dialogTitle = "New Task"
   }
 
   ngOnInit(): void {
-    this.tasksList = [{id: 1, name: "New task name 1"}, {id: 2, name: "New task name 2"}]
+    this.tasksList = [{ id: 1, name: "New task name 1" }, { id: 2, name: "New task name 2" }]
     this.sendTasksList();
   }
 
@@ -46,19 +49,19 @@ export class TasksComponent implements OnInit, OnDestroy {
   // }
 
 
-  showOrHideTaskDialog(title: string, fields: string[]) {
+  newTask(title: string, fields: string[]): void {
     this.showTaskMenu = !this.showTaskMenu;
     const dialogRef = this.dialog.open(DialogFormComponent)
     //const dialogRef = this.dialog.open(DialogFormComponent, { title: title })
     dialogRef.componentInstance.title = title
-    dialogRef.componentInstance.fields = [title, 'priority', 'time']
+    //dialogRef.componentInstance.fields = [title, 'priority', 'time']
     fields.forEach(field => {
       dialogRef.componentInstance.fields = [field]
     });
-    dialogRef.componentInstance.formSubmit.subscribe((formValue: any) => {
+    this.newTaskSub = dialogRef.componentInstance.formSubmit.subscribe((formValue: any) => {
       const lastTask = this.tasksList[this.tasksList.length - 1];
       let newTask: Task = { id: 0, name: formValue.name };
-      if(lastTask != undefined){
+      if (lastTask != undefined) {
         newTask.id = lastTask.id + 1;
       } else {
         newTask.id = 1;
@@ -67,23 +70,24 @@ export class TasksComponent implements OnInit, OnDestroy {
       console.log(newTask.id)
       this.sendTasksList();
     });
-    //dialogRef.componentInstance.formSubmit.unsubscribe
-
-
-    // dialogRef.afterClosed().subscribe(result =>  {
-    //   console.log("Dialog output:", result)
-    //   this.sendTasksList();
-    // })
   }
 
-  ngOnDestroy(){
+  editTask(title: string, task: Task, fields: string[]): void {
+    this.showTaskMenu = !this.showTaskMenu;
+    const dialogRef = this.dialog.open(DialogFormComponent)
+    dialogRef.componentInstance.title = title
+    fields.forEach(field => {
+      dialogRef.componentInstance.fields = [field]
+    });
+    this.dialogEditSub = dialogRef.componentInstance.formSubmit.subscribe((formValue: any) => {
+      task.name = formValue.name
+    });
 
   }
 
-  editTask() {
-    console.log('Edit task placeholder')
-    alert("add task placeholder");
-    this.sendTasksList();
+  ngOnDestroy() {
+    this.newTaskSub.unsubscribe();
+    this.dialogEditSub.unsubscribe();
   }
 
   clearInput(index: number) {
